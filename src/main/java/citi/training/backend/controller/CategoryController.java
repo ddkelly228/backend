5b94658e3ae136e566f684461641c8c3773334f9
wjq-hhh <50319421+wjq-hhh@users.noreply.github.com>
2020-09-12 22:50:25 +0800

    Update CategoryController.java

--- src/main/java/citi/training/backend/controller/CategoryController.java
+++ src/main/java/citi/training/backend/controller/CategoryController.java
@@ -1,10 +1,27 @@
 package citi.training.backend.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+import citi.training.backend.model.Category;
+import citi.training.backend.serviceImpl.CategoryServiceImpl;
+import citi.training.backend.utils.ResultUtils;
 
 @RestController
 public class CategoryController {
 	
+	@Autowired
+	private CategoryServiceImpl categoryServiceImpl;
 	
+	@GetMapping(value="/categories")
+	public JSONObject getCategories() {
+		List<Category> list = categoryServiceImpl.getCategories();
+		return ResultUtils.success(list);
+	}
 
 }
